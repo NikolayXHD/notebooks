8585219a05dc26d6babd9e45985b910b20227c2f
NikolayXHD <hidalgo1984@list.ru>
2026-05-19 18:25:26 +0300

    ubuntu 24

--- components/example-notebook-servers/ssh-proxy-custom/common.sh
+++ components/example-notebook-servers/ssh-proxy-custom/common.sh
@@ -25,13 +25,17 @@ set_pip_index_url() {
 }
 
 install_common_packages() {
-    apt -y update && apt-get install -y python3-pip p7zip-full p7zip-rar build-essential man-db mc htop nvtop nvidia-cuda-toolkit
+    apt -y update && apt-get install -y python3-pip p7zip-full p7zip-rar build-essential man-db mc htop nvtop
+    wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+    dpkg -i cuda-keyring_1.1-1_all.deb
+    apt-get update
+    apt-get install -y cuda-toolkit-12-6
     unminimize
     apt clean
 }
 
 install_common_python_packages() {
-    pip install --no-cache-dir \
+    pip install --break-system-packages --no-cache-dir \
         poetry==1.8.5
 }
 
